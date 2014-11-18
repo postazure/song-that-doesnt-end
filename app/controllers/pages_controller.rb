8e06f402e066406b7e5962dd93afa34ad0578caa
postazure <gordonmaxc@gmail.com>
2014-11-18 11:58:49 -0800

    Completed w/strech

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
+  def verse_2
+     render "index"
+  end
 
+  def verse_3
+     render "index"
+  end
+
+  def verse_4
+     render "index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "pages#index", as: :home
+  get "verse-2" => "pages#verse_2"
+  get "verse-3" => "pages#verse_3"
+  get "verse-4" => "pages#verse_4"
+
 end
